b00c15de12fa9b3e2fcb3c0f300f8e4dac023368
Cosmin Stretea <49166578+Quesmin@users.noreply.github.com>
2020-12-27 18:01:37 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBBOEooZcM8bsTOBVa_XhdozHdK5xDyWsU",
-    authDomain: "instagram-clone-5edcb.firebaseapp.com",
-    databaseURL: "https://instagram-clone-5edcb.firebaseio.com",
-    projectId: "instagram-clone-5edcb",
-    storageBucket: "instagram-clone-5edcb.appspot.com",
-    messagingSenderId: "658989295361",
-    appId: "1:658989295361:web:1ef9065079b04c2aa89d00"
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    databaseURL: DB_URL,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: SENDER_ID,
+    appId: APP_ID
 });
 
 const db = firebaseApp.firestore();
